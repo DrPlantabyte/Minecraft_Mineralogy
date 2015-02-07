35354125b43be5119b8e94ab6b45efd72ab8dc3a
CCHall <explosivegnome@yahoo.com>
2015-02-06 19:56:34 -0500

    added dirt walking noise

--- src/main/java/cyano/mineralogy/blocks/Soil.java
+++ src/main/java/cyano/mineralogy/blocks/Soil.java
@@ -14,6 +14,7 @@ public class Soil extends Block{
 	public Soil(String unlocalizedName){
         super(Material.ground);
         this.setUnlocalizedName(unlocalizedName);
+        this.setStepSound(Block.soundTypeGravel);
 		this.setHarvestLevel("shovel", 0);
 		this.setHardness(0.5f); // dirt is 0.5, grass is 0.6, stone is 1.5,iron ore is 3, obsidian is 50
 		this.setResistance(0f); // dirt is 0, iron ore is 5, stone is 10, obsidian is 2000
