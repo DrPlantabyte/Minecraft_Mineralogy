bc7af536fb0eeed8a4de54d4b13cca64ebf51508
Chris Hall <explosivegnome@yahoo.com>
2016-04-16 19:14:27 -0400

    fixed dimension bug

--- src/main/java/cyano/mineralogy/worldgen/StoneReplacer.java
+++ src/main/java/cyano/mineralogy/worldgen/StoneReplacer.java
@@ -1,13 +1,13 @@
 package cyano.mineralogy.worldgen;
 
-import java.util.Random;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
+import cyano.mineralogy.Mineralogy;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.fml.common.IWorldGenerator;
-import cyano.mineralogy.Mineralogy;
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class StoneReplacer implements IWorldGenerator {
 
@@ -36,7 +36,9 @@ public class StoneReplacer implements IWorldGenerator {
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world,
 			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-		getGeology(world).replaceStoneInChunk(chunkX, chunkZ, world);
+		if(world.provider.getDimensionId() == 0){
+			getGeology(world).replaceStoneInChunk(chunkX, chunkZ, world);
+		}
 	}
 
 }
