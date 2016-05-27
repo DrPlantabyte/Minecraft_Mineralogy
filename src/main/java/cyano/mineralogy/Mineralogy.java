53e76d9a7db3d5edb481fbbbcbeea39159dd46d9
Chris Hall <explosivegnome@yahoo.com>
2016-05-26 20:41:01 -0400

    update to MC 1.9.4

--- src/main/java/cyano/mineralogy/Mineralogy.java
+++ src/main/java/cyano/mineralogy/Mineralogy.java
@@ -39,7 +39,7 @@ public class Mineralogy
 {
     public static final String MODID = "mineralogy";
     public static final String NAME ="Mineralogy";
-    public static final String VERSION = "3.0.2";
+    public static final String VERSION = "3.0.3";
     /** stone block replacesments that are sedimentary */
     public static final List<Block> sedimentaryStones = new ArrayList<Block>();
     /** stone block replacesments that are metamorphic */
